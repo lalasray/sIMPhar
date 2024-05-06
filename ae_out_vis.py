7a1491b72f30174fb0692b9acdd271132b8a0d32
Lala Shakti Swarup Ray <lalas@port-3128.kl.dfki.de>
2024-05-06 14:22:42 +0200

    Update ae_out_vis.py

--- ae_out_vis.py
+++ ae_out_vis.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import torch
 import numpy as np
 
-pickle_file_path = r"C:\Users\lalas\Desktop\sIMphar\out\S5_1.pt"
+pickle_file_path = r"C:\Users\lalas\Desktop\sIMphar\out\S10_1.pt"
 
 loaded_data = torch.load(pickle_file_path)
 
@@ -19,11 +19,11 @@ axs[1].set_title('real mag')
 axs[1].grid(True)
 
 axs[2].plot(loaded_data["sIMP_phase"])
-axs[2].set_title('real phase')
+axs[2].set_title('synth phase')
 axs[2].grid(True)
 
 axs[3].plot(loaded_data["IMP_phase"])
-axs[3].set_title('real mag')
+axs[3].set_title('real phase')
 axs[3].grid(True)
 
 axs[4].plot(loaded_data["class"])
