e4ba194ea99e733cefdf3e4c55d89a633e6bc6fb
Lala Shakti Swarup Ray <lalas@port-3128.kl.dfki.de>
2024-05-07 15:04:50 +0200

    Update Dataloader.py

--- Dataloader.py
+++ Dataloader.py
@@ -7,9 +7,32 @@ from torch.nn.utils.rnn import pad_sequence
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-#batch_size = 32
-#data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+class TriDataset(Dataset):
+    def __init__(self, data_files):
+        self.data_files = data_files
 
-#for pose, imu, text in data_loader:
-    #print(pose.shape, imu.shape, text.shape)
-    #break
+    def __len__(self):
+        return len(self.data_files)
+
+    def __getitem__(self, idx):
+        data = torch.load(self.data_files[idx])
+        imp = data[0]
+        text = data[1]
+        return imp,text
+
+def get_data_files(data_path):
+    data_files = []
+    for file in os.listdir(data_path):
+        if file.endswith('.pt'):
+            data_files.append(os.path.join(data_path, file))
+    return data_files
+
+dataset_train = TriDataset(get_data_files(r"D:\Dataset\Meta\simphar\sIMphar_synth\processed"))
+
+batch_size = 32
+data_loader = DataLoader(dataset_train, batch_size=batch_size, shuffle=True)
+
+for imp,text in data_loader:
+    print(imp.shape)
+    print(text.shape)
+    break
